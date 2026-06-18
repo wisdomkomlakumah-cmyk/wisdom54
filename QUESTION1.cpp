247e4a17a047981929152808728d70afdadc0711
wisdomkomlakumah-cmyk <wisdomkomlakumah@gmail.com>
2026-06-18 17:33:40 +0000

    Add files via upload

---
+++ QUESTION1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Sum = " << num1 + num2 << endl;
+    cout << "Difference = " << num1 - num2 << endl;
+    cout << "Product = " << num1 * num2 << endl;
+    cout << "Quotient = " << num1 / num2 << endl;
+
+    return 0;
+}
